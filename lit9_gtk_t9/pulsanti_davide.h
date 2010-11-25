8cfbf26dd2485a66ac79ca0ca4f4c5b8716ee970
n.peditto <n.peditto@8fdded58-2856-4509-2145-962a5f3090a9>
2010-11-25 15:44:51 +0000

    We included t9 code and added remote control's mapping

---
+++ lit9_gtk_t9/pulsanti_davide.h
@@ -0,0 +1,67 @@
+/*******************************************************************
+	       mioTelecomando(2fd)   Konig(ff0)		LG-Nico
+ch_minus		F807		7bec
+ch_plus			D827		7bed
+power			48B7		7bf3		28D7
+blue			D22D		7ba1
+red			12ED		7ba4
+green			926D		7ba3
+yellow                  52AD		7ba2
+down			B847		7be0		916E
+up			9867		7be1		41BE
+right			02FD		7bde		09F6
+left			42BD		7bdf		C13E
+ok			847B		7bdd		718E
+tasto_1			807F		7bfe		A05F
+tasto_2			40BF		7bfd		609F
+tasto_3			C03F		7bfc		E01F
+tasto_4			20DF		7bfb		30CF
+tasto_5			A05F		7bfa		609F
+tasto_6			609F		7bf9		708F
+tasto_7                 E01F		7bf8		F00F
+tasto_8                 10EF		72fdc23dbf7		38C7		
+tasto_9                 906F		7bf6		B847	
+tasto_0                 00FF		7bff		20DF
+vol_plus		58A7		7bef
+vol_minus		7887		7bee
+tasto_exit              C23D		7bf1
+********************************************************************/
+#ifndef __pulsanti_davide_h__
+#define __pulsanti_davide_h__
+
+/*Codice*/
+#define ch_minus 	"2fdF807"
+#define ch_plus	 	"2fdD827"
+#define power	 	"2fd48B7"
+#define blue		"2fdD22D"
+#define red		"2fd12ED"
+#define green		"2fd926D"
+#define yellow		"2fd52AD"
+#define down		"2fdB847"
+#define up		"2fd9867"
+#define right		"2fd02FD"
+#define left		"2fd42BD"
+#define ok		"2fd847B"
+#define tasto_1		"2fd807F"
+#define tasto_2		"2fd40BF"
+#define tasto_3		"2fdC03F"
+#define tasto_4		"2fd20DF"
+#define tasto_5		"2fdA05F"
+#define tasto_6		"2fd609F"
+#define tasto_7       	"2fdE01F"
+#define tasto_8       	"2fd10EF"
+#define tasto_9       	"2fd906F"
+#define tasto_0       	"2fd00FF"
+#define vol_plus	"2fd58A7"
+#define vol_minus	"2fd7887"
+#define tasto_exit	"2fdC23D"
+
+
+#endif
+
+
+
+
+
+
+
