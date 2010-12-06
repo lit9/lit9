c0114310853a1dc305ee6304860cb10da46fdb83
davidemulfari@gmail.com <davidemulfari@gmail.com@8fdded58-2856-4509-2145-962a5f3090a9>
2010-12-06 18:32:46 +0000

    LIT is based on SQLite database

---
+++ lit_sql/pulsanti.h
@@ -0,0 +1,71 @@
+/*******************************************************************
+	       mioTelecomando(2fd)   Konig(ff0)		LG-Nico
+ch_minus		F807		7bec
+ch_plus			D827		7bed
+power			48B7		7bf3		28D7
+blue			D22D		7ba1
+red			12ED		7ba4
+green			926D		7ba3
+yellow                  52AD		7ba2
+down			B847		7be0		916E
+up			9867		7be1		41BE
+right			02FD		7bde		09F6
+left			42BD		7bdf		C13E
+ok			847B		7bdd		718E
+tasto_1			807F		7bfe		A05F
+tasto_2			40BF		7bfd		609F
+tasto_3			C03F		7bfc		E01F
+tasto_4			20DF		7bfb		30CF
+tasto_5			A05F		7bfa		609F
+tasto_6			609F		7bf9		708F
+tasto_7                 E01F		7bf8		F00F
+tasto_8                 10EF		7bf7		38C7		
+tasto_9                 906F		7bf6		B847	
+tasto_0                 00FF		7bff		20DF
+vol_plus		58A7		7bef
+vol_minus		7887		7bee
+tasto_exit              C23D		7bf1
+mute_clear				7bf5
+Record        				7be8
+********************************************************************/
+#ifndef __pulsanti_h__
+#define __pulsanti_h__
+
+/*Codice*/
+#define ch_minus 	"ff07bec"
+#define ch_plus	 	"ff07bed"
+#define power	 	"ff07bf3"
+#define blue		"ff07ba1"
+#define red		"ff07ba4"
+#define green		"ff07ba3"
+#define yellow		"ff07ba2"
+#define down		"ff07be0"
+#define up		"ff07be1"
+#define right		"ff07bde"
+#define left		"ff07bdf"
+#define ok		"ff07bdd"
+#define tasto_1		"ff07bfe"
+#define tasto_2		"ff07bfd"
+#define tasto_3		"ff07bfc"
+#define tasto_4		"ff07bfb"
+#define tasto_5		"ff07bfa"
+#define tasto_6		"ff07bf9"
+#define tasto_7       	"ff07bf8"
+#define tasto_8       	"ff07bf7"
+#define tasto_9       	"ff07bf6"
+#define tasto_0       	"ff07bff"
+#define vol_plus	"ff07bef"
+#define vol_minus	"ff07bee"
+#define tasto_exit	"ff07bf1"
+#define mute_clear      "ff07bf5"
+#define Record        	"ff07be8"
+
+
+#endif
+
+
+
+
+
+
+
