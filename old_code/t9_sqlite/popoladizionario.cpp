8dd19f24d845846a55a7cde1f8367d549ab4f53e
carmelo.romeo85 <carmelo.romeo85@8fdded58-2856-4509-2145-962a5f3090a9>
2011-01-11 17:33:21 +0000

    some corrections on the db and popoladizionario.cpp

--- old_code/t9_sqlite/popoladizionario.cpp
+++ old_code/t9_sqlite/popoladizionario.cpp
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+//Per le estensioni dell'ASCII (caratteri estesi->vocali accentate)
+#include <wchar.h>
+
 int main(int argc, char** args)
 {
     // Create an int variable for storing the return code for each call
@@ -32,44 +35,78 @@ int main(int argc, char** args)
 
     char querydel[100] = "DELETE FROM italiano";
     retval = sqlite3_exec(handle,querydel,0,0,0);
+	
 
+    //Mapping per i caratteri accentati
+    wchar_t wcarattere[6];     // variabile per carattere esteso
+
+    wcarattere[0]= 224;  //à = 224    0xE0
+    wcarattere[1]= 232;  //è = 232    0xE8
+    wcarattere[2]= 233;  //é = 233    0xE9
+    wcarattere[3]= 236;  //ì = 236    0xEC
+    wcarattere[4]= 242;  //ò = 242    0xF2
+    wcarattere[5]= 249;  //ù = 249    0xF9
+
+    printf("prova\n");
+    printf("wcarattere[5] = '%lc'\n",wcarattere[5]);
+    printf("fine prova\n");
 
     FILE *f=NULL;
     char codice[100];
     char parola[100];
-   char queryins1[70]="INSERT INTO italiano (codice,parola)VALUES('";
-   char queryins2[6]="','";
-   char queryins3[6]="')";
-   char *query;
-   int i=0;
-   query=(char*)malloc(sizeof(char)*230);
-   for(i=0; i<230; i++) query[i] = '\0';
+    char query[300];
 
-    f=fopen("dizionario_ita.txt","r");
+    wchar_t aux[100];
 
-	while (!feof(f))
-	{
-	fscanf(f,"%s %s",codice,parola);
-	   strcat( query, queryins1);
-	   strcat( query, codice);
-	   strcat( query, queryins2);
-	   strcat( query, parola);
-	   strcat( query, queryins3);
-   retval = sqlite3_exec(handle,query,0,0,0);
-	if(retval)
+    bzero(query,300);
+
+    f=fopen("b.txt","r");
+/*
+    while (!feof(f))
     {
-        printf("ERRORE\n");
-        return -1;
-    }
-//   printf("%s\n",query);
-   for(i=0; i<230; i++) query[i] = '\0';
+	fscanf(f,"%s %s",codice, parola);
+
+	for(int i=0; i<100; i++)
+	{
+	    if(parola[i]==(char)wcarattere[0])
+		aux[i]= (char)wcarattere[0];
+
+	    else if(parola[i]==(char)wcarattere[1])
+		aux[i]= (char)wcarattere[1];
+
+	    else if(parola[i]==(char)wcarattere[2])
+		aux[i]= (char)wcarattere[2];
+
+	    else if(parola[i]==(char)wcarattere[3])
+		aux[i]= (char)wcarattere[3];
+
+	    else if(parola[i]==(char)wcarattere[4])
+		aux[i]= (char)wcarattere[4];
+
+	    else if(parola[i]==(char)wcarattere[5])
+		aux[i]= (char)wcarattere[5];
+
+	    else aux[i]=parola[i];
 	}
 
-	fclose(f);
-	printf("FATTO\n");
-   sqlite3_close(handle);
+	//sprintf(query, "INSERT INTO italiano (codice, parola) VALUES('%s','%s')", codice, parola);
+	sprintf(query, "INSERT INTO italiano (codice, parola) VALUES('%s','%ls')", codice, aux);
 
+        retval = sqlite3_exec(handle,query,0,0,0);
+	if(retval)
+        {
+            printf("ERRORE\n");
+            return -1;
+        }
+	printf("%s\n",query);
 
+        bzero(query,300);
+    }
+
+    fclose(f);
+    printf("FATTO\n");
+    sqlite3_close(handle);
+*/
     return 0;
 	
 }
